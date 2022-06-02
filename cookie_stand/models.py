70c2f0333f2de1c14c4e243d0741f38709257b37
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-02 15:50:00 -0700

    Docker init

--- cookie_stand/models.py
+++ cookie_stand/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 
 class CookieStand(models.Model):
-    name = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
     owner = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
-    description = models.TextField(default="", null=True, blank=True)
-
+    description = models.TextField(blank=True)
+    hourly_sales = models.JSONField(default=list, null=True)
+    minimum_customers_per_hour = models.IntegerField(default=0)
+    maximum_customers_per_hour = models.IntegerField(default=0)
+    average_cookies_per_sale = models.FloatField(default=0)
+    
     def __str__(self):
-        return self.name
+        return self.location
